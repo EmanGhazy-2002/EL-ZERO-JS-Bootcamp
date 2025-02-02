33827afdcc1f3cb19ac932fac4145cc0d9f3736e
Eman Ghazy <ghazyeman220@gmail.com>
2025-02-02 15:02:51 +0200

    task4

---
+++ week-04-Numbers[23-26]/assignment-01.js
@@ -0,0 +1,12 @@
+console.log( 100_000 ); // 100000
+console.log( 100000 ); // 100000
+console.log( 5e4 + 5e4 ); // 100000
+console.log( 1e5 ); // 100000
+console.log(10 ** 5); // 100000
+console.log(100000.0); // 100000
+console.log(+"100000"); // 100000
+console.log(Number("100000")); // 100000
+console.log(parseInt("100000")); // 100000
+console.log(parseFloat("100000")); // 100000
+console.log( Math.pow(10,5) ); // 100000
+console.log( Math.trunc(100000.99)) // 100000
