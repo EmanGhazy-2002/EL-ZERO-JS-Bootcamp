b90412279552f1b7dddcaaf8b677e1725aeebf29
Eman Ghazy <ghazyeman220@gmail.com>
2025-02-02 16:15:31 +0200

    task7

---
+++ week-07-IfCondition[33-37]/assignment-02.js
@@ -0,0 +1,22 @@
+let num1 = 9;
+let str = "9";
+let str2 = "20";
+
+if(num1==str)
+    console.log("{num1} Is The Same Value As {str}");
+else if(num1==str){ 
+    if(typeof(num1)!==typeof(str))
+        console.log("{num1} Is The Same Value As {str} But Not The Same Type");
+    }
+//line 3
+if(num1 != str2){
+    if(typeof num1 !== typeof str){
+      console.log("{num1} Is Not The Same Value Or The Same Type As {str2}");
+    }
+  }
+  //Line 4
+if(typeof str === typeof str2){
+    if(str !== str2){
+      console.log("{str} Is The Same Type As {str2} But Not The Same Value");
+    }
+  }
