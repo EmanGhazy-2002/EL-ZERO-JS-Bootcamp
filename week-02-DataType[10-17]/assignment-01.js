4baf8645ac82a8a46c772a8ea2c1b29414f107c1
Eman Ghazy <ghazyeman220@gmail.com>
2025-02-02 13:49:45 +0200

    task2

---
+++ week-02-DataType[10-17]/assignment-01.js
@@ -0,0 +1,13 @@
+let numberOne=10;
+let numberTwo=20;
+
+// Ouput
+console.log(numberOne+""+numberTwo); // Normal Concatenate => 1020
+console.log(typeof(numberOne+""+numberTwo)); // Normal Concatenate => String
+console.log(`${numberOne}${numberTwo}`); // Template Literals Way => 1020
+console.log(typeof(`${numberOne}${numberTwo}`)); // Template Literals Way => String
+
+console.log(numberOne+"\n"+numberTwo);
+
+console.log(`${numberTwo}
+${numberOne}`);
